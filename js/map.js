e41cecd4a2470bd02aa9249d3881e0fe2ecefb77
İlkay Can <83129148+gameonstreet@users.noreply.github.com>
2021-06-27 23:54:27 +0300

    Create js folder and map.js

---
+++ js/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FtZW9uc3RyZWV0IiwiYSI6ImNrcWY2aHk2djB1MWgydXBlZ2g0YmY4YnEifQ.uJr1PkESet5r94fnNnwOCw';
+    var map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/gameonstreet/cknqn5nvm04wk17uf29j4u064', // style URL
+        center: [27.854587, 40.453905], // starting position [LNG, LAT]
+        zoom: 10 // starting zoom
+    });
