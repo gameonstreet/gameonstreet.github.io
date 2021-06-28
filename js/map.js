c79e9ac5cdc01b7ae3f59b748869fffbaa462c32
İlkay Can <83129148+gameonstreet@users.noreply.github.com>
2021-06-28 08:57:54 +0300

    Update map.js
    Add the lines for Mapbox geolocate control

--- js/map.js
+++ js/map.js
@@ -15,3 +15,13 @@ map.addControl(
         language: 'tr-TR' // specify the language as Turkish.
     })
 );
+
+// Add geolocate control to the map.
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    })
+);
