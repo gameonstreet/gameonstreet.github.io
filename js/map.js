1e8ccf00439f0fd1d57570e97378c1aeafbc42e4
İlkay Can <83129148+gameonstreet@users.noreply.github.com>
2021-06-28 23:54:33 +0300

    Update map.js
    Add pop-up to each POI

--- js/map.js
+++ js/map.js
@@ -45,3 +45,37 @@ map.on('load', function () {
         }
     });
 });
+
+// When a click event occurs on a feature in the places layer, open a popup at the
+// location of the feature, with description HTML from its properties.
+map.on('click', 'battery-layer', function (e) {
+    var coordinates = e.features[0].geometry.coordinates.slice();
+    var title = e.features[0].properties.title;
+    var address = e.features[0].properties.address;
+    var phone = e.features[0].properties.phone;
+    var province = e.features[0].properties.province;
+    var city = e.features[0].properties.city;
+    var description = '<strong>' + title + '</strong>' + '<p>' + address + ' ' + province + '/' + city + '</p>' + '<p>' + phone + '</p>';
+     
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+     
+    new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map);
+});
+
+// Change the cursor to a pointer when the mouse is over the places layer.
+map.on('mouseenter', 'battery-layer', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+ 
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'battery-layer', function () {
+    map.getCanvas().style.cursor = '';
+});
