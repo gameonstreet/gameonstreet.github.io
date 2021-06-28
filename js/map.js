bc3df81b18ca15b3da7ef6ae2208ccc32c60bdf8
İlkay Can <83129148+gameonstreet@users.noreply.github.com>
2021-06-28 16:29:34 +0300

    Update map.js
    Adding data source from external geojson file

--- js/map.js
+++ js/map.js
@@ -25,3 +25,23 @@ map.addControl(
         trackUserLocation: true
     })
 );
+
+map.on('load', function () {
+    map.addSource('battery', {
+        type: 'geojson',
+        // use a URL for the value for the `data` property.
+        data: 'https://raw.githubusercontent.com/gameonstreet/mapbox-geojson/main/battery_location.geojson'
+    });
+    // for more information about this section please visit the link below. If the link is dead you can email me at gameonstreet@gmail.com
+    // https://gis.stackexchange.com/questions/179323/mapbox-studio-how-to-upload-glyphs-or-sprite-for-custom-markers
+    map.addLayer({
+        'id': 'battery-layer',
+        'type': 'symbol',
+        'source': 'battery',
+        'layout': {
+            'icon-image': '{marker-symbol}', // markers are hosted on MapBox Studio
+            'icon-allow-overlap': true,
+            'icon-size': 0.7
+        }
+    });
+});
